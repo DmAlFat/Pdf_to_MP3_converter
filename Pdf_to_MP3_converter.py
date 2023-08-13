8d678b4e8673c6de184452e5059b065f33c51c43
DmAlFat <fateev8292@gmail.com>
2023-08-14 00:37:02 +0300

    add threading and v.2.2

--- Pdf_to_MP3_converter.py
+++ Pdf_to_MP3_converter.py
@@ -4,6 +4,7 @@ from gtts import gTTS
 import pdfplumber
 from pathlib import Path
 from tkinter.messagebox import showerror, showinfo
+from threading import Thread
 
 root = Tk()
 root.geometry("477x80+750+400")
@@ -25,7 +26,7 @@ def dismiss(window):
 def click():
     window = Toplevel()
     window.title("PDF to MP3 converter")
-    window.geometry("290x200+840+400")
+    window.geometry("290x250+840+400")
     window.iconphoto(False, icon)
     window.protocol("WM_DELETE_WINDOW", lambda: dismiss(window))
     label = ttk.Label(window, text="1) Select language | Default: English")
@@ -45,7 +46,7 @@ def click():
     combobox = ttk.Combobox(window, values=list(languages), state="readonly")
     combobox.pack(anchor=N, fill=X, padx=5, pady=5)
     combobox.bind("<<ComboboxSelected>>", selected)
-    label4 = ttk.Label(window, text="2) Select pdf file and the path to the saving file")
+    label4 = ttk.Label(window, text="2) Select pdf file and the path to the saving file\n      The conversion will start automatically")
     label4.pack(anchor=N, padx=5, pady=5)
 
     def open_info():
@@ -68,16 +69,29 @@ def click():
             file_name = Path(filepath).stem
             file_directory = filedialog.askdirectory()
             mp3.save(f'{file_directory}\{file_name}.mp3')
-
+            progressbar.stop()
             return open_info()
         else:
+            progressbar.stop()
             return open_error()
 
-    close_button = ttk.Button(window, text="Select pdf file and the path to the saving file", command=Pdf_to_MP3_converter)
+    def start():
+        progressbar.start(25)
+
+    def merge():
+        thread1 = Thread(target=start)
+        thread2 = Thread(target=Pdf_to_MP3_converter)
+        thread1.start()
+        thread2.start()
+
+
+    close_button = ttk.Button(window, text="Select...", command=merge)
     close_button.pack(anchor="center", expand=1)
+    progressbar = ttk.Progressbar(window, orient="horizontal", mode="indeterminate")
+    progressbar.pack(fill=X, padx=5, pady=5)
     label5 = ttk.Label(window, text="3) Wait for the conversion to complete...")
     label5.pack(anchor=N, padx=5, pady=5)
-    label2 = ttk.Label(window, text="v.2.1")
+    label2 = ttk.Label(window, text="v.2.2")
     label2.pack(anchor=SW)
     label1 = ttk.Label(window, text="Developed by DmAlFat")
     label1.pack(anchor=SE)
