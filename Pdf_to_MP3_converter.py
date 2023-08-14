297a5df917b1056323f13e8274ae0565b046fee6
DmAlFat <fateev8292@gmail.com>
2023-08-14 19:27:53 +0300

    add commemts and PEP8

--- Pdf_to_MP3_converter.py
+++ Pdf_to_MP3_converter.py
@@ -12,13 +12,15 @@ root.title("PDF to MP3 converter")
 icon = PhotoImage(file="icon.png")
 root.iconphoto(False, icon)
 
-label = ttk.Label(text="This program is developed to convert PDF file to MP3 file.\nAttention! An Internet connection is required for the correct operation of the program.")
+label = ttk.Label(
+    text="This program is developed to convert PDF file to MP3 file.\n"
+         "Attention! An Internet connection is required for the correct operation of the program.")
 label.pack(anchor=N, padx=10, pady=10)
 
-language_input = 'en'
+language_input = 'en'  # create a global variable to display the default language
 
 
-def dismiss(window):
+def dismiss(window):  # Definition of a function to intercept the closing of the launch window
     window.grab_release()
     window.destroy()
 
@@ -29,16 +31,17 @@ def click():
     window.geometry("290x250+840+400")
     window.iconphoto(False, icon)
     window.protocol("WM_DELETE_WINDOW", lambda: dismiss(window))
-    label = ttk.Label(window, text="1) Select language | Default: English")
-    label.pack(anchor=N)
+    label0 = ttk.Label(window, text="1) Select language | Default: English")
+    label0.pack(anchor=N)
 
-
-    def selected(event):
+    def selected(event):  # defining the language selection function from combox and changing the global variable
         global language_input
         language_input = languages[combobox.get()]
         label3["text"] = f"Selected language: {combobox.get()}"
 
+    # dictionary to select value
     languages = {'English': "en", 'Русский': "ru", 'French': 'fr', 'Portuguese': 'pt', 'Spanish': 'es'}
+    # reverse dictionary for key selection
     inverse_languages = {v: k for k, v in languages.items()}
 
     label3 = ttk.Label(window, text=f"Selected language: {inverse_languages[language_input]}")
@@ -46,7 +49,9 @@ def click():
     combobox = ttk.Combobox(window, values=list(languages), state="readonly")
     combobox.pack(anchor=N, fill=X, padx=5, pady=5)
     combobox.bind("<<ComboboxSelected>>", selected)
-    label4 = ttk.Label(window, text="2) Select pdf file and the path to the saving file\n      The conversion will start automatically")
+    label4 = ttk.Label(window,
+                       text="2) Select pdf file and the path to the saving file\n"
+                            "      The conversion will start automatically")
     label4.pack(anchor=N, padx=5, pady=5)
 
     def open_info():
@@ -56,8 +61,8 @@ def click():
         showerror(title="Error", message="Wrong file's format, check the file's format!")
 
     def Pdf_to_MP3_converter():
-        filepath = filedialog.askopenfilename()
-        if Path(filepath).is_file() and Path(filepath).suffix == '.pdf':
+        filepath = filedialog.askopenfilename()  # file path selection
+        if Path(filepath).is_file() and Path(filepath).suffix == '.pdf':  # Test file selection and format
 
             with pdfplumber.PDF(open(file=filepath, mode='rb')) as pdf:
                 pages = [page.extract_text() for page in pdf.pages]
@@ -65,9 +70,9 @@ def click():
             text = ''.join(pages)
             text = text.replace('\n', '')
 
-            mp3 = gTTS(text=text, lang=language_input.strip())
+            mp3 = gTTS(text=text, lang=language_input)  # resort to API google
             file_name = Path(filepath).stem
-            file_directory = filedialog.askdirectory()
+            file_directory = filedialog.askdirectory()  # saving file path selection
             mp3.save(f'{file_directory}\{file_name}.mp3')
             progressbar.stop()
             return open_info()
@@ -78,12 +83,11 @@ def click():
     def start():
         progressbar.start(25)
 
-    def merge():
-        thread1 = Thread(target=start)
+    def merge():  # create function to execute threads
+        thread1 = Thread(target=start)  # creating a class Thread with a called function
         thread2 = Thread(target=Pdf_to_MP3_converter)
-        thread1.start()
+        thread1.start()  # method call for stream start
         thread2.start()
-
 
     close_button = ttk.Button(window, text="Select...", command=merge)
     close_button.pack(anchor="center", expand=1)
@@ -102,4 +106,3 @@ open_button = ttk.Button(text="Start", command=click)
 open_button.pack(anchor="center", expand=1)
 
 root.mainloop()
-
